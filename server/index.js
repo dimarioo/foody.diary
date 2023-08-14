6cf1923a089d2394ee830e2c95c7f5713f65a3d5
Dimario Oliver <Dimario Oliver>
2023-08-14 17:27:33 -0400

    Database Connected, Authenthication Working, Recieving Data

--- server/index.js
+++ server/index.js
@@ -1,10 +1,35 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const User = require('./Models/User')
+const bcrypt = require('bcrypt')
+const salt = bcrypt.genSaltSync(10)
 
 
 const app = express ()
 
-const port = process.env.PORT || 3000
 
-app.use(express.static('public'))
-app.use(express.urlencoded({ extended: true }))
+
+app.use(cors());
+app.use(express.json())
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true, 
+    useUnifiedTopology: true
+  })
+app.post('/signup', async (req,res) =>{
+    const {username, password} = req.body
+    try{
+        const userDoc = await User.create({username, password:bcrypt.hashSync(password,salt)})
+        res.json(userDoc)
+    }catch(e){
+        res.status(400).json(e)
+    }
+
+    
+} )
+
+
+app.listen(3001)
+
