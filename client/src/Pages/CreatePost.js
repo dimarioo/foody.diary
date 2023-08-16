7a4e5716d234d539431610eed8cbb6163956ba37
Dimario Oliver <Dimario Oliver>
2023-08-16 19:06:19 -0400

    Post Page Up and Running , Route Working , working on rendering

--- client/src/Pages/CreatePost.js
+++ client/src/Pages/CreatePost.js
@@ -23,15 +23,38 @@ export default function CreatePost(){
     const [title, setTitle] = useState('')
     const [summary,setSummary] = useState('')
     const [content,setContent] = useState('')
+    const [files , setFiles] = useState('')
+    async function createNewPost(e){
+        const data = new FormData()
+        data.set('title', title)
+        data.set('summart', summary)
+        data.set('content', content)
+        data.set('file', files[0])
 
+        e.preventDefault()
+        const response =  await fetch('http://localhost:3001/post',{
+            method: 'POST',
+            body: data,
+            
+    })
+    console.log(await response.json())
+} 
 
     return(
-        <form>
-          <input type="title" placeholder={'Title'} value={title} onChange={e => setTitle(e.target.value)} />
-          <input type="summary" placeholder={'Summary'} />
-          <input type="file"/>
-          <ReactQuill value={content} modules={modules} formats={formats}/>
+    <form onSubmit={createNewPost}>
+        <input type="title" placeholder={'Title'}
+           value={title}
+           onChange={e => setTitle(e.target.value)} />
+        <input type="summary" placeholder={'Summary'} 
+          value={summary} 
+          onChange={e => setSummary(e.target.value)}/>
+        <input type="file"
+        onChange={e => setFiles(e.target.files)}/>
+        <ReactQuill value={content}
+          onChange={newValue => setContent(newValue)}
+           modules={modules} 
+           formats={formats}/>
           <button style={{marginTop:'5px'}}>Create Post</button>
-        </form>
+     </form>
     )
 }
