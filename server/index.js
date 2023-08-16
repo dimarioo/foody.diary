69391b3c178ecd9ac3d877440e9d618b738ab2d2
Dimario Oliver <Dimario Oliver>
2023-08-16 19:24:47 -0400

    Database Connected, Authenthication Working, Recieving Data, Form Data Captured

--- server/index.js
+++ server/index.js
@@ -66,13 +66,22 @@ app.post('/logout', (req,res) => {
     res.cookie('token', '').json('ok')
 })
 
-app.post('/post', uploadMiddleware.single('file'), (req,res) => {
+app.post('/post', uploadMiddleware.single('file'),  async (req,res) => {
     const {originalname, path} = req.file
     const parts = originalname.split('.')
     const ext = parts[parts.length - 1]
     const newPath = path+'.' +ext
-    fs.renameSync(newPath)
+    fs.renameSync(path, newPath)
    
+    const {title, summary, content} = req.body
+    const postDoc = await Post.create({
+        title, 
+        summary,
+        content,
+        cover: newPath,
+    })
+
+    res.json({postDoc})
 })
 
 
