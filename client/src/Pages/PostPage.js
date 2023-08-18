04dae85740bce34f40eaf16ce5aa7390abbb312b
Dimario Oliver <Dimario Oliver>
2023-08-18 17:02:58 -0400

    Post Page Up and Running , Route Working ,  Single Page Post In Progress

--- client/src/Pages/PostPage.js
+++ client/src/Pages/PostPage.js
@@ -1,5 +1,26 @@
+import { useEffect, useState } from "react"
+import { useParams } from "react-router-dom"
+
 export default function PostPage(){
+    const [postInfo, setPostInfo] = useState(null)
+    const{id} = useParams()
+    useEffect (() => {
+        fetch(`http://localhost:3001/post/${id}`)
+        .then(response => {
+            response.json().then(postInfo =>{
+                setPostInfo(postInfo)
+            })
+        })        
+    }, [])
+
+    if(!postInfo) return ''
  return(
-    <div>Post Page</div>
+    <div className="post-page">
+       <div className="image">
+        <img src={`http://localhost:3001/${postInfo.cover}`} alt=""/>
+       </div>
+       {/* <h1>{postInfo.title}</h1> */}
+       {/* <div dangerouslySetInnerHTML={{__html:postInfo.content}}/> */}
+    </div>
  )
 }
