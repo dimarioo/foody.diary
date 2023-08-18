83ae1e7f6c7ad2ea59f9ad713e2bc14525481ab1
Dimario Oliver <Dimario Oliver>
2023-08-18 16:29:25 -0400

    Post Page Up and Running , Route Working ,  Single Page Post In Progress

--- client/src/Components/Post.js
+++ client/src/Components/Post.js
@@ -1,16 +1,23 @@
 import {formatISO9075} from 'date-fns'
+import { Link } from 'react-router-dom'
 
-export default function Post({title, summary , cover, content, createdAt}){
+export default function Post({_id, title, summary , cover, content, createdAt, author}){
     return(
     <main>
       <div className='entry'>
         <div className='image'>
-          <img src='https://www.rudysislandgrill.com/cdn/shop/products/Screenshot_20220323-212717.jpg?v=1648096143' alt=''/>
+          <Link to={`/post/${_id}`}>
+          <img src={'http://localhost:3001/' + cover} alt=''/>
+          </Link>
+          
         </div>
         <div className='info'>
-        <h2>{title}</h2>
+           <Link to={`/post/${_id}`}>
+           <h2>{title}</h2>
+           </Link>
+        
         <p className='userinfo'>
-          <a className='author'>Dimario Oliver</a>
+          <a className='author'>{author.username}</a>
           <time>{formatISO9075(new Date(createdAt))}</time>
         </p>
         <p className='summary'>{summary}</p>
