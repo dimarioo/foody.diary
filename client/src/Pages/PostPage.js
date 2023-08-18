6582fb21567fccccd51c411d99a621aa84886b04
Dimario Oliver <Dimario Oliver>
2023-08-18 17:17:28 -0400

    Post Page Up and Running , Route Working ,  Single Page Post Working

--- client/src/Pages/PostPage.js
+++ client/src/Pages/PostPage.js
@@ -18,7 +18,8 @@ export default function PostPage(){
  return(
     <div className="post-page">
         <h1>{postInfo.title}</h1>
-        {/* <time>{formatISO9075(new Date(postInfo.createdAt))}</time> */}
+        <time>{formatISO9075(new Date(postInfo.createdAt))}</time>
+        <div className="author">by {postInfo.author.username}</div>
         <div className="image">
             <img src={`http://localhost:3001/${postInfo.cover}`} alt=""/>
         </div>
